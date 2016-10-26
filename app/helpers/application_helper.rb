ce4b064e4238c232a794fc7076c5c2056cfead62
Remus Rusanu <contact@rusanu.com>
2016-10-26 14:16:39 +0000

    apparently works (projects, memebers)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,18 @@
 module ApplicationHelper
 
+  def glyphicon_tag(glyph)
+    content_tag(:span, "",  class: "glyphicon glyphicon-#{glyph}")
+  end
+
   def glyphicon_submit_helper(glyph)
     button_tag(type: 'submit', class: 'btn btn-primary') do
-      content_tag(:span, "",  class: "glyphicon glyphicon-#{glyph}")
+      glyphicon_tag(glyph)
+    end
+  end
+
+  def glyphicon_link_to(url, glyph='play-circle')
+    link_to url, class: 'btn btn-link' do
+      glyphicon_tag(glyph)
     end
   end
 
@@ -12,6 +22,13 @@ module ApplicationHelper
       url = 'http://' + url
     end
     return url
+  end
+
+  def detect_profile_path(profile)
+    return coordinator_path(profile) if profile.is_coordinator?
+    return fellow_path(profile) if profile.is_fellow?
+    return volunteer_path(profile) if profile.is_volunteer?
+    return applicant_path(profile) if profile.is_applicant?
   end
 
 end
