14923609bd8ba3a423e15bb4cd3d6065cb5f76aa
Cristian Mircea Messel <mess110@gmail.com>
2016-11-24 19:49:51 +0200

    cleanup meeting_spec
    because we include FactoryGirl in the test enviornment, we can just call
    create instead of FactoryGirl.create

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Meeting, type: :model do
+  let(:meeting) { create :meeting }
+
   it 'is valid with valid attributes' do
-    expect(FactoryGirl.create(:meeting)).to be_valid
+    expect(meeting).to be_valid
   end
+
   it 'is invalid without a date' do
-    expect(FactoryGirl.build(:meeting, date: nil)).to be_invalid
+    expect(build(:meeting, date: nil)).to be_invalid
   end
+
   it 'is invalid without a location' do
-    expect(FactoryGirl.build(:meeting, location: nil)).to be_invalid
+    expect(build(:meeting, location: nil)).to be_invalid
   end
 end
