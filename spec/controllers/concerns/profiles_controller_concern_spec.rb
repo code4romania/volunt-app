e03c9f1c572f080e66c24695564b6fcdefdcf77a
Mircea Mare <shotgundebugging@users.noreply.github.com>
2016-12-07 22:22:11 +0200

    Test GET VolunteerController#new

--- spec/controllers/concerns/profiles_controller_concern_spec.rb
+++ spec/controllers/concerns/profiles_controller_concern_spec.rb
@@ -5,19 +5,32 @@ describe VolunteersController, type: :controller do
     session[:user_id] = {
       "id"    => user.id,
       "email" => user.email,
-      "level" => user.flags
+      "level" => LoginConcern::USER_LEVEL_NEWUSER
     }
   end
 
-  before { sign_in volunteer }
+  before           { sign_in volunteer }
+  let!(:volunteer) { create(:volunteer) }
 
-  let!(:volunteer)      { create(:volunteer) }
-  let(:profile)         { Profile.find_by_email(volunteer.email) }
-  let(:project)         { create(:project) }
-  let!(:memberships)    { profile.memberships = [create(:project_member, profile: profile, project: project)] }
-  let!(:status_reports) { profile.status_reports = [create(:status_report, profile: profile, project: project)] }
+  describe 'GET #new' do
+    let(:profile)    { Profile.find_by_email(volunteer.email) }
+
+    it 'sets profile flag and assigns profile with current email' do
+      get :new, { params: { protocol: 'https' } }
+      expect(profile.flags).to eq(volunteer.flags)
+      expect(profile.email).to eq(volunteer.email)
+      expect(assigns(:profile)).to be_an_instance_of(Profile)
+      expect(assigns(:profile).email).to eq(volunteer.email)
+      expect(response).to render_template('profiles/new')
+    end
+  end
 
   describe 'GET #show' do
+    let(:profile)         { Profile.find_by_email(volunteer.email) }
+    let(:project)         { create(:project) }
+    let!(:memberships)    { profile.memberships = [create(:project_member, profile: profile, project: project)] }
+    let!(:status_reports) { profile.status_reports = [create(:status_report, profile: profile, project: project)] }
+
     it 'assigns profile and project members' do
       get :show, { params: { id: volunteer.id, protocol: 'https' } }
       expect(assigns(:profile)).to eq(profile)
--- spec/factories/volunteer.rb
+++ spec/factories/volunteer.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :volunteer, class: User do
     email 'volunteer@example.com'
-    flags LoginConcern::USER_LEVEL_VOLUNTEER
+    flags Profile::PROFILE_FLAG_VOLUNTEER
 
     after(:create) do |volunteer|
       create(:profile, email: volunteer.email, flags: Profile::PROFILE_FLAG_VOLUNTEER)
