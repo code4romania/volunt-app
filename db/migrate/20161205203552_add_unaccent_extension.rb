4804ba9f97a493450af7826f40a4d6acaf0df932
Remus Rusanu <contact@rusanu.com>
2016-12-07 19:20:17 +0000

    superuser unaccent

--- db/migrate/20161205203552_add_unaccent_extension.rb
+++ db/migrate/20161205203552_add_unaccent_extension.rb
@@ -1,10 +1,14 @@
 # https://www.postgresql.org/docs/9.1/static/unaccent.html
 class AddUnaccentExtension < ActiveRecord::Migration[5.0]
   def up
-    execute "create extension unaccent"
+    # This requires superuser. If it fails with permission problems,
+    # thou shall enable unaccent manually and then run the migration which will be a no-op
+    enable_extension :unaccent unless extension_enabled?(:unaccent)
   end
 
   def down
-    execute "drop extension unaccent"
+    # If you wish to disable the unaccent extension, do it so manually
+    # removing it is problematic due to superuser requirements
   end
+
 end
