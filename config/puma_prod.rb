58499369af4218ed5be7473781cddd765d1023d1
Lucian Ursu <lucian.ursu@gmail.com>
2017-01-25 11:50:25 +0200

    Add a production config file for puma

---
+++ config/puma_prod.rb
@@ -0,0 +1,21 @@
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_count, threads_count
+
+app_dir = File.expand_path("../..", __FILE__)
+tmp_dir = "#{app_dir}/tmp"
+
+# Default to production
+rails_env = ENV.fetch('RAILS_ENV') { "production" }
+environment rails_env
+
+# Listen on
+bind "unix://#{tmp_dir}/sockets/puma.sock"
+
+# Logging
+stdout_redirect "#{tmp_dir}/log/puma.stdout.log", "#{tmp_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{tmp_dir}/pids/puma.pid"
+state_path "#{tmp_dir}/pids/puma.state"
+activate_control_app
+
