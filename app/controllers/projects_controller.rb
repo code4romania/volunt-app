666a90104017462cfdd48a8e000a0ac79fb99664
Vitalie Lazu <vitalie.lazu@gmail.com>
2017-03-07 21:21:36 +0200

    User can join a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,10 +18,10 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   def show
     @status_reports = @project.status_reports.paginate(page: params[:status_reports_page])
-    @volunteers = @project.members.volunteers.includes(:profile).paginate(page: params[:volunteers_page])
-    @fellows = @project.members.fellows.includes(:profile).paginate(page: params[:fellows_page])
+    @volunteers = @project.members.includes(:profile).paginate(page: params[:volunteers_page])
     openings = is_user_level_fellow? ? @project.openings : @project.openings.visible
     @openings = openings.paginate(page: params[:openings_page])
+
     if is_user_level_fellow?
       render 'show'
     else
