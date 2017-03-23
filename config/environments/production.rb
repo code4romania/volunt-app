c743a7b7ebb5ff9f9479a4f2afd1682031a132f1
Lucian Ursu <lucian.ursu@gmail.com>
2017-03-23 16:33:49 +0200

    Add mail config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  config.action_mailer.delivery_method = :sendmail
+
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+      :address => ENV["EMAIL_HOST"],
+      :port => ENV["EMAIL_PORT"],
+      :user_name => ENV["EMAIL_USERNAME"],
+      :password => ENV["EMAIL_PASSWORD"],
+      :authentication => :login,
+      :enable_starttls_auto => true
+  }
 end
