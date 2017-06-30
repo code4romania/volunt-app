cf982cdb65f214dc5a1ca117e463b8e2dfb8d3df
Lucian Ursu <lucian.ursu@gmail.com>
2017-06-30 18:46:12 +0300

    Fix more puma paths

--- config/puma_prod.rb
+++ config/puma_prod.rb
@@ -16,7 +16,7 @@ bind "unix://#{tmp_dir}/puma/puma.sock"
 stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{tmp_dir}/pids/puma.pid"
-state_path "#{tmp_dir}/pids/puma.state"
+pidfile "#{tmp_dir}/puma/puma.pid"
+state_path "#{tmp_dir}/puma/puma.state"
 activate_control_app
 
