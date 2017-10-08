807a190790e07f8e6471448e36e9ef0fa8924cca
Blecher Eyal <blechereyal@gmail.com>
2017-10-08 17:47:53 +0300

    inheritance structure for profile policies

---
+++ app/policies/profile_policy.rb
@@ -0,0 +1,41 @@
+class ProfilePolicy < ApplicationPolicy
+  attr_reader :user, :profile
+
+  def initialize(user, profile)
+    @user = user
+    @profile = profile
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    profile.user_id = user.id
+  end
+
+  def search?
+    true
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+end
